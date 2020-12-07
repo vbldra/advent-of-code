d5ffd4caf30933ab362addc02163fa6d6eebcd85
ilona <ilona.avdiushkina@gmail.com>
2020-12-07 17:39:38 +0100

    day 7, part 1

---
+++ 7_day.js
@@ -0,0 +1,38 @@
+const { connected } = require('process');
+fs = require('fs');
+fs.readFile('7_input', 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    let array = data.split("\n");
+    let stringArray = [];
+    let bags = {};
+    for (let i = 0; i < array.length; i++) {
+        stringArray.push(array[i].split(" "))
+    }
+    for (let i = 0; i < stringArray.length; i++) {
+        currentBagBig = stringArray[i][0] + " " + stringArray[i][1];
+        bags[currentBagBig] = {};
+        for (let j = 7; j < stringArray[i].length; j += 4) { 
+            bags[currentBagBig][stringArray[i][j-2] + " " + stringArray[i][j-1]] = Number(stringArray[i][j-3]);
+        }
+    }
+    let countSet = new Set();
+    function addToSet(object, string) {
+        for (const key in object) {
+            if (object.hasOwnProperty(key)) {
+                for (const key1 in object[key]) {
+                    if (key1 === string) {
+                        countSet.add(key);
+                    }
+                }
+            }
+        }
+    }
+    addToSet(bags, "shiny gold");
+    for (const iterator of countSet) {
+        addToSet(bags, iterator);
+    }
+    console.log(countSet.size)
+    
+});
