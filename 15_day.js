2e1c2045e1742a375d2e115b32ba10f519ef7795
ilona <ilona.avdiushkina@gmail.com>
2020-12-16 00:00:29 +0100

    15 day 2 part

--- 15_day.js
+++ 15_day.js
@@ -1,4 +1,4 @@
-const ARRAY = [0,13,1,8,6,15];
+let ARRAY = [0,13,1,8,6,15];
 let gameArray = [...ARRAY];
 let countIndex = 2020;
 let currentNumber = gameArray[gameArray.length-1];
@@ -15,6 +15,38 @@ function countNextStep(index, currentNumber, array) {
 for (let i = gameArray.length; i < countIndex; i++) {
     currentNumber = countNextStep(i - 1, currentNumber, gameArray);
     gameArray.push(currentNumber);
+    if (i % 10000 === 0) {
+        console.log(i);
+    }
+}
+
+console.log(currentNumber);
+
+// 2
+
+let gameObj = {};
+for (let i = 0; i < ARRAY.length - 1; i++) {
+    gameObj[ARRAY[i]] = i;
+}
+
+countIndex = 30000000 - 1;
+
+function countNextStep(index, currentNumber, obj) {
+    if (Object.hasOwnProperty.call(obj, currentNumber)) {
+        return index - obj[currentNumber];
+    }
+    return 0;
+}
+
+currentNumber = ARRAY[ARRAY.length-1]
+
+for (let i = ARRAY.length-1; i < countIndex; i++) {
+    let nextNumber = countNextStep(i, currentNumber, gameObj);
+    gameObj[currentNumber] = i;
+    currentNumber = nextNumber
+    if (i % 500000 === 0) {
+        console.log(i);
+    }
 }
 
 console.log(currentNumber);
