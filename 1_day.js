9ce7230d5262fdd49e92f55916d957cf4b95bbf1
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2021-12-01 20:27:22 +0100

    first solved

--- 1_day.js
+++ 1_day.js
@@ -1,49 +1,45 @@
-const { connected } = require('process');
-fs = require('fs');
-fs.readFile('1_input', 'utf8', function (err,data) {
-    if (err) {
-        return console.log(err);
-    }
-    
-    let arrayOfStrings = data.split(`\n`);
-    
-    let arrayOfNumbers = [];
-    // Converting array of string to array of numbers
-    for (let i = 0; i < arrayOfStrings.length; i++) {
-        arrayOfNumbers.push(Number(arrayOfStrings[i]))
-    }
+const { connected } = require("process");
+fs = require("fs");
+fs.readFile("1_input", "utf8", function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  let arrayOfStrings = data.split(`\n`);
+  let arrayOfNumbers = [];
+  // Converting array of string to array of numbers
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    arrayOfNumbers.push(Number(arrayOfStrings[i]));
+  }
 
-    // First task
-    function findSumOfTwo2020(array) {
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 1; j < array.length; j++) {
-                if (array[i] + array[j] === 2020) {
-                    console.log(array[i], array[j])
-                    return (array[i] * array[j])
-                }
-            }
-        }
+  // First task
+  function lagerMeasurements(array) {
+    let measurements = {
+      decreased: 0,
+      increased: 0,
+    };
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < array[i - 1]) {
+        measurements["decreased"]++;
+      } else if (array[i] > array[i - 1]) {
+        measurements["increased"]++;
+      } 
     }
-    console.log(findSumOfTwo2020(arrayOfNumbers));
-    
-    // Second task
-    function findSumOfThree2020(array) {
-        for (let i = 0; i < array.length; i++) {
-            for (let j = 1; j < array.length; j++) {
-                for (let h = 2; h < array.length; h++) {
-                    if (array[i] + array[j] + array[h] === 2020) {
-                        console.log(array[i], array[j], array[h]);
-                        return (array[i] * array[j] * array[h]);
-                    }
-                }
-            }
-        }
+    console.log(measurements);
+    let values = Object.values(measurements);
+    return Math.max(...values);
+  }
+  console.log("---=== 1 task ===---");
+  console.log("Maximum: " + lagerMeasurements(arrayOfNumbers));
+
+  // Second task
+  function lagerSumMeasurements(array) {
+    let arrayOfSum = [];
+    for (let i = 0; i < array.length - 2; i++) {
+      let sumOfThree = array[i] + array[i + 1] + array[i + 2];
+      arrayOfSum.push(sumOfThree)
     }
-    console.log(findSumOfThree2020(arrayOfNumbers));
+    return(lagerMeasurements(arrayOfSum));
+  }
+  console.log("---=== 2 task ===---");
+  console.log("Maximum of sum: " + lagerSumMeasurements(arrayOfNumbers));
 });
-  
-
-
-
-
-
