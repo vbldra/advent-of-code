eefaeea92c382f7f56409ceb0a913345a84cbfa5
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2021-12-08 17:47:50 +0100

    6th after 256 days

--- 6_day.js
+++ 6_day.js
@@ -4,45 +4,42 @@ fs.readFile("6_input.txt", "utf8", function (err, data) {
     return console.log(err);
   }
   let arrayOfStrings = data.split(",");
-  // Converting array of string to array of numbers
-  let fishes = [];
-  // Converting array of string to array of numbers
-  for (let i = 0; i < arrayOfStrings.length; i++) {
-    fishes.push(Number(arrayOfStrings[i]));
-  }
-  let days = 80
+  let fishes = arrayOfStrings.sort().map(Number);
+  // Creating array to store amount of fishes sorted by days from 0 to 8
+  let counting = Array(9).fill(0);
+  fishes.forEach((fish) => {
+    counting[fish]++;
+  });
+  console.log(counting);
+  let days = 80;
 
-  function oneDay(fishes) {
-    let newFishes = []
-    let isNewFish = []
-    fishes.forEach(fish => {
-      if (fish == 0) {
-        isNewFish.push(8)
-        newFishes.push(6)
-      } else {
-        newFishes.push(fish-1)
+  function oneDay(counting) {
+    let newCounting = Array(9).fill(0);
+    counting.forEach((count, index) => {
+      if (index == 0 ) {
+        newCounting[6] += count;
+        newCounting[8] += count;
+      } else if (index > 0) {
+        newCounting[index - 1] += count;
       }
     });
-    if (isNewFish.length > 0) {
-      newFishes = [...newFishes, ...isNewFish]
-    }
-    return newFishes
+    return newCounting
   }
-  function result(fishes) {
-    let after = [...fishes]
-    let count = 0
-    while (count < days) {
-      after = oneDay(after)
-      count++
+  function result(counting) {
+    let initial = 1;
+    while (initial <= days) {
+      counting = oneDay(counting);
+      initial++;
     }
-    return after.length
+    let sum = counting.reduce((acc, a) => acc + Number(a), 0);
+    return sum;
   }
-
-  
 
   console.log("---=== 1 task ===---");
-  console.log("After 80 days: " + result(fishes));
+  console.log("After 80 days: " + result(counting));
 
   // Second task
   console.log("---=== 2 task ===---");
+  days = 256
+  console.log("After 256 days: " + result(counting));
 });
