34b25005912b7f05e657ba815a6f60f31641d84a
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2022-12-01 23:09:02 +0100

    react app init

---
+++ README.old.md
@@ -0,0 +1,4 @@
+# AdventOfCode
+[Link to Advent Of Code 2021](https://adventofcode.com/2021)
+
+Solved using JavaScript
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Advent of code 2022
+    </div>
+  );
+}
+
+export default App;
