2ffbdfe6f41b7dec0258ee8612955044602d90e5
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2022-12-11 17:33:14 +0100

    click to copy result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Result(props) {
   return (
     <div className="Result">
-      <p>1 task: {props.result[0]}</p>
-      <p>2 task: {props.result[1]}</p>
+      <p onClick={()=>navigator.clipboard.writeText(props.result[0])}>1 task: {props.result[0]}</p>
+      <p onClick={()=>navigator.clipboard.writeText(props.result[1])}>2 task: {props.result[1]}</p>
     </div>
   );
 }
