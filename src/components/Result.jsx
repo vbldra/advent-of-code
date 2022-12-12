5bf392efb9ec7f058b377346ba0b76ccf1c99a5c
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2022-12-12 21:38:18 +0100

    copy result to clipboard

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,8 +3,12 @@ import React from "react";
 function Result(props) {
   return (
     <div className="Result">
-      <p onClick={()=>navigator.clipboard.writeText(props.result[0])}>1 task: {props.result[0]}</p>
-      <p onClick={()=>navigator.clipboard.writeText(props.result[1])}>2 task: {props.result[1]}</p>
+      <p onClick={() => navigator.clipboard.writeText(props.result[0])}>
+        1 task: {props.result[0] ? props.result[0] : "no answer"}
+      </p>
+      <p onClick={() => navigator.clipboard.writeText(props.result[1])}>
+        2 task: {props.result[1] ? props.result[1] : "no answer"}
+      </p>
     </div>
   );
 }
