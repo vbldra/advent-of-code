c37b3864e42bb384bd54c6f353114dbab2850d12
Ilona Avdiushkina <ilona.avdiushkina@gmail.com>
2022-12-14 21:14:39 +0100

    styling for not-number results

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,14 +1,40 @@
 import React from "react";
 
 function Result(props) {
+  let answers = [];
+  for (let i = 0; i < 2; i++) {
+    console.log(props.result[i]);
+    if (props.result[i].length > 0 && typeof props.result[i] !== "number") {
+      let line = "";
+      for (let j = 0; j < props.result[i].length; j++) {
+        line = line + props.result[i][j].join("") + "\n";
+      }
+      answers[i] = (
+        <div>
+          <pre>
+            <code dangerouslySetInnerHTML={{ __html: line }} />
+          </pre>
+        </div>
+      );
+    } else {
+      answers[i] = props.result[i];
+    }
+  }
+
   return (
     <div className="Result">
-      <p onClick={() => navigator.clipboard.writeText(props.result[0])}>
-        1 task: {props.result[0] ? props.result[0] : "no answer"}
-      </p>
-      <p onClick={() => navigator.clipboard.writeText(props.result[1])}>
-        2 task: {props.result[1] ? props.result[1] : "no answer"}
-      </p>
+      {answers.map((result, i) => {
+        return (
+          <div
+            className="result"
+            onClick={() => navigator.clipboard.writeText(result)}
+            key={+i + 1}
+          >
+            {i + 1} task:
+            {result ? result : "no answer"}
+          </div>
+        );
+      })}
     </div>
   );
 }
